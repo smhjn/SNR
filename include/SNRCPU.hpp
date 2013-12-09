e94e0e388358da474cb3cff4c4962d8276df9cb9
Alessio Sclocco <alessio@sclocco.eu>
2013-12-09 14:48:44 +0100

    Implemented the transpose sequential algorithm.

--- include/SNRCPU.hpp
+++ include/SNRCPU.hpp
@@ -1,32 +1,23 @@
-/*
- * Copyright (C) 2013
- * Alessio Sclocco <a.sclocco@vu.nl>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
+//
+// Copyright (C) 2013
+// Alessio Sclocco <a.sclocco@vu.nl>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
 
-#include <string>
-#include <vector>
 #include <cmath>
-#include <x86intrin.h>
-#include <omp.h>
-using std::string;
-using std::vector;
-using std::make_pair;
-using std::pow;
-using std::ceil;
+using std::sqrt;
 
 #include <Observation.hpp>
 using AstroData::Observation;
@@ -37,23 +28,21 @@ using AstroData::Observation;
 
 namespace PulsarSearch {
 
-// OpenMP SNR
+// Sequential SNR
 /*template< typename T > void transientSNR(Observation< T > & observation, CLData< T > * input, CLData< T > * output);*/
 template< typename T > void pulsarSNR(const Observation< T > & observation, const T * const __restrict__ foldedData, T * const __restrict__ snrs);
 
 
 // Implementation
 template< typename T > void pulsarSNR(const Observation< T > & observation, const T * const __restrict__ foldedData, T * const __restrict__ snrs) {
-	#pragma omp parallel for
-	for ( unsigned int dm = 0; dm < observation.getNrDMs(); dm++ ) {
-		#pragma omp parallel for
-		for ( unsigned int period = 0; period < observation.getNrPeriods(); period++ ) {
+	for ( unsigned int period = 0; period < observation.getNrPeriods(); period++ ) {
+		for ( unsigned int dm = 0; dm < observation.getNrDMs(); dm++ ) {
 			T max = 0;
 			float average = 0.0f;
 			float rms = 0.0f;
 
 			for ( unsigned int bin = 0; bin < observation.getNrBins(); bin++ ) {
-				T value = foldedData[( ( ( dm * observation.getNrPeriods() ) + period ) * observation.getNrPaddedBins() ) + bin];
+				T value = foldedData[(bin * observation.getNrPeriods() * observation.getNrPaddedDMs()) + (period * observation.getNrPaddedDMs()) + dm];
 
 				average += value;
 				rms += (value * value);
@@ -65,7 +54,7 @@ template< typename T > void pulsarSNR(const Observation< T > & observation, cons
 			average /= observation.getNrBins();
 			rms = sqrt(rms / observation.getNrBins());
 
-			snrs[( dm * observation.getNrPaddedPeriods() ) + period] = ( max - average) / rms;
+			snrs[(period * observation.getNrPaddedDMs()) + dm] = (max - average) / rms;
 		}
 	}
 }
