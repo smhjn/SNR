16a4bdae6092fc45e2383b15a2514cf28973ef14
Alessio Sclocco <alessio@sclocco.eu>
2014-08-26 17:24:16 +0200

    Added the AVX code generation. Still a work in progress.

--- include/SNR.hpp
+++ include/SNR.hpp
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 #include <string>
+#include <x86intrin.h>
 
 #include <utils.hpp>
 #include <Observation.hpp>
@@ -26,7 +27,10 @@ namespace PulsarSearch {
 // Sequential SNR
 template< typename T > void snrFoldedTS(const AstroData::Observation< T > & observation, const std::vector< T > & foldedTS, std::vector< T > & snrs);
 // OpenCL SNR
-template< typename T > std::string getSNROpenCL(const unsigned int nrDMsPerBlock, const unsigned int nrPeriodsPerBlock, const unsigned int nrDMsPerThread, const unsigned int nrPeriodsPerThread, std::string & dataType, const AstroData::Observation< T > & observation);
+template< typename T > std::string * getSNROpenCL(const unsigned int nrDMsPerBlock, const unsigned int nrPeriodsPerBlock, const unsigned int nrDMsPerThread, const unsigned int nrPeriodsPerThread, std::string & dataType, const AstroData::Observation< T > & observation);
+// AVX SNR
+std::string * getSNRAVX(const unsigned int nrDMsPerThread, const unsigned int nrPeriodsPerThread);
+// Phi SNR
 
 
 // Implementations
@@ -55,7 +59,7 @@ template< typename T > void snrFoldedTS(AstroData::Observation< T > & observatio
 	}
 }
 
-template< typename T > std::string getSNROpenCL(const unsigned int nrDMsPerBlock, const unsigned int nrPeriodsPerBlock, const unsigned int nrDMsPerThread, const unsigned int nrPeriodsPerThread, std::string & dataType, const AstroData::Observation< T > & observation) {
+template< typename T > std::string * getSNROpenCL(const unsigned int nrDMsPerBlock, const unsigned int nrPeriodsPerBlock, const unsigned int nrDMsPerThread, const unsigned int nrPeriodsPerThread, std::string & dataType, const AstroData::Observation< T > & observation) {
   std::string * code = new std::string();
 
   // Begin kernel's template
@@ -70,7 +74,8 @@ template< typename T > std::string getSNROpenCL(const unsigned int nrDMsPerBlock
     + dataType + " rms = 0;\n"
     + dataType + " max = 0;\n"
     "\n"
-    "<%COMPUTE%>";
+    "<%COMPUTE%>"
+    "}\n";
     std::string defDMsTemplate = "const unsigned int dm<%DM_NUM%> = (get_group_id(0) * " + isa::utils::toString< unsigned int >(nrDMsPerBlock * nrDMsPerThread) + ") + get_local_id(0) + <%DM_NUM%>;\n";
   std::string defPeriodsTemplate = "const unsigned int period<%PERIOD_NUM%> = (get_group_id(1) * " + isa::utils::toString< unsigned int >(nrPeriodsPerBlock * nrPeriodsPerThread) + ") + get_local_id(1) + <%PERIOD_NUM%>;\n";
   std::string computeTemplate = "average = 0;\n"
@@ -84,8 +89,7 @@ template< typename T > std::string getSNROpenCL(const unsigned int nrDMsPerBlock
     "}\n"
     "average *= " + nrBinsInverse_s + "f;\n"
     "rms *= " + nrBinsInverse_s + "f;\n"
-    "snrs[((period + <%PERIOD_NUM%>) * " + nrPaddedDMs_s + ") + dm + <%DM_NUM%>] = (max - average) / native_sqrt(rms);\n"
-    "}\n";
+    "snrs[((period + <%PERIOD_NUM%>) * " + nrPaddedDMs_s + ") + dm + <%DM_NUM%>] = (max - average) / native_sqrt(rms);\n";
   // End kernel's template
 
   std::string * defDM_s = new std::string();
@@ -120,6 +124,62 @@ template< typename T > std::string getSNROpenCL(const unsigned int nrDMsPerBlock
 
   code = isa::utils::replace(code, "<%DEF_DM%>", *defDM_s, true);
   code = isa::utils::replace(code, "<%DEF_PERIOD%>", *defPeriod_s, true);
+  code = isa::utils::replace(code, "<%COMPUTE%>", *compute_s, true);
+
+  return code;
+}
+
+std::string * getSNRAVX(const unsigned int nrDMsPerThread, const unsigned int nrPeriodsPerThread) {
+  std::string * code = new std::string();
+
+  // Begin kernel's template
+  *code = "namespace PulsarSearch {\n"
+    "template< typename T > void snr" + isa::utils::toString(nrDMsPerThread) + "x" + isa::utils::toString(nrPeriodsPerThread) + "(const AstroData::Observation< T > & observation, const float * const __restrict__ foldedData, float * const __restrict__ snrs) {\n"
+    "{\n"
+    "__m256 max = _mm256_setzero_ps();\n"
+    "__m256 average = _mm256_setzero_ps();\n"
+    "__m256 rms = _mm256_setzero_ps();\n"
+    "#pragma omp parallel for schedule(static)\n"
+    "for ( unsigned int period = 0; period < observation.getNrPeriods(); period += " + isa::utils::toString(nrPeriodsPerThread) + ") {\n"
+    "#pragma omp parallel for schedule(static)\n"
+    "for ( unsigned int dm = 0; dm < observation.getNrDMs(); dm += " + isa::utils::toString(nrPeriodsPerThread) + " * 8) {\n"
+    "<%COMPUTE%>"
+    "}\n"
+    "}\n"
+    "}\n"
+    "}\n"
+    "}\n";
+  std::string computeTemplate = "max = _mm256_setzero_ps();\n"
+    "average = _mm256_setzero_ps();\n"
+    "rms = _mm256_setzero_ps();\n"
+    "\n"
+    "for ( unsigned int bin = 0; bin < observation.getNrBins(); bin++ ) {\n"
+    "__m256 value = _mm256_load_ps(&(foldedData[(bin * observation.getNrPeriods() * observation.getNrPaddedDMs()) + ((period + <%PERIOD_NUM%>) * observation.getNrPaddedDMs()) + dm + <%DM_NUM%>]));\n"
+    "\n"
+    "average = _mm256_add_ps(average, value);\n"
+    "rms = _mm256_add_ps(rms, _mm256_mul_ps(value, value));\n"
+    "max = _mm256_max_ps(max, value);\n"
+    "}\n"
+    "average = _mm256_div_ps(average, _mm256_set1_ps(observation.getNrBins()));\n"
+    "rms = _mm256_sqrt_ps(_mm256_div_ps(rms, _mm256_set1_ps(observation.getNrBins())));\n"
+    "_mm256_store_ps(&(snrs[((period + <%PERIOD_NUM%>) * " + nrPaddedDMs_s + ") + dm + <%DM_NUM%>]),_mm256_div_ps(_mm256_sub_ps(max, average), rms));\n";
+  // End kernel's template
+
+  std::string * compute_s = new std::string();
+
+  for ( unsigned int period = 0; period < nrPeriodsPerThread; period++ ) {
+    std::string period_s = isa::utils::toString< unsigned int >(period);
+
+    for ( unsigned int dm = 0; dm < nrDMsPerThread; dm++ ) {
+      std::string dm_s = isa::utils::toString< unsigned int >(dm);
+      std::string * temp_s = 0;
+
+      temp_s = isa::utils::replace(&computeTemplate, "<%PERIOD_NUM%>", period_s);
+      temp_s = isa::utils::replace(temp_s, "<%DM_NUM%>", dm_s, true);
+      compute_s->append(*temp_s);
+      delete temp_s;
+    }
+  }
   code = isa::utils::replace(code, "<%COMPUTE%>", *compute_s, true);
 
   return code;
