035240b74845d892a22b37c71ac172373f577e4d
Alessio Sclocco <alessio@sclocco.eu>
2014-08-27 15:31:24 +0200

    OpenCL test.

--- tests/SNR.cpp
+++ tests/SNR.cpp
@@ -1,4 +1,4 @@
-// Copyright 2012 Alessio Sclocco <a.sclocco@vu.nl>
+// Copyright 2014 Alessio Sclocco <a.sclocco@vu.nl>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -13,128 +13,142 @@
 // limitations under the License.
 
 #include <iostream>
-using std::cout;
-using std::cerr;
-using std::endl;
 #include <string>
-using std::string;
 #include <vector>
-using std::vector;
 #include <exception>
-using std::exception;
+#include <fstream>
 #include <iomanip>
-using std::fixed;
-using std::setprecision;
 #include <limits>
-using std::numeric_limits;
-#include <cmath>
 #include <ctime>
 
 #include <ArgumentList.hpp>
-using isa::utils::ArgumentList;
+#include <Exceptions.hpp>
 #include <Observation.hpp>
-using AstroData::Observation;
 #include <InitializeOpenCL.hpp>
-using isa::OpenCL::initializeOpenCL;
-#include <CLData.hpp>
-using isa::OpenCL::CLData;
+#include <Kernel.hpp>
 #include <utils.hpp>
-using isa::utils::same;
 #include <SNR.hpp>
-using PulsarSearch::SNR;
-#include <SNRCPU.hpp>
-using PulsarSearch::pulsarSNR;
 
 typedef float dataType;
-const string typeName("float");
+std::string typeName("float");
 
 
 int main(int argc, char *argv[]) {
+  bool print = false;
 	unsigned int clPlatformID = 0;
 	unsigned int clDeviceID = 0;
-	unsigned int nrDMsPerBlock = 0;
-	unsigned int nrPeriodsPerBlock = 0;
-	long long unsigned int wrongValues = 0;
-	Observation< dataType > observation("SNRTest", typeName);
-	CLData< dataType > foldedData("FoldedData", true);
-	CLData< dataType > SNRData("SNRData", true);
-	CLData< dataType > SNRDataCPU("SNRDataCPU", true);
+  unsigned int nrDMsPerBlock = 0;
+  unsigned int nrPeriodsPerBlock = 0;
+  unsigned int nrDMsPerThread = 0;
+  unsigned int nrPeriodsPerThread = 0;
+	long long unsigned int wrongSamples = 0;
+	AstroData::Observation< dataType > observation("SNRTest", typeName);
 
 	try {
-		ArgumentList args(argc, argv);
-
+    isa::utils::ArgumentList args(argc, argv);
+    print = args.getSwitch("-print");
 		clPlatformID = args.getSwitchArgument< unsigned int >("-opencl_platform");
 		clDeviceID = args.getSwitchArgument< unsigned int >("-opencl_device");
-
+    observation.setPadding(args.getSwitchArgument< unsigned int >("-padding"));
+    nrDMsPerBlock = args.getSwitchArgument< unsigned int >("-db");
+    nrPeriodsPerBlock = args.getSwitchArgument< unsigned int >("-pb");
+    nrDMsPerThread = args.getSwitchArgument< unsigned int >("-dt");
+    nrPeriodsPerThread = args.getSwitchArgument< unsigned int >("-pt");
 		observation.setNrDMs(args.getSwitchArgument< unsigned int >("-dms"));
-		observation.setNrPeriods(args.getSwitchArgument< unsigned int >("-periods"));
-		observation.setNrBins(args.getSwitchArgument< unsigned int >("-bins"));
-
-		nrDMsPerBlock = args.getSwitchArgument< unsigned int >("-p1");
-		nrPeriodsPerBlock = args.getSwitchArgument< unsigned int >("-p2");
-	} catch ( exception &err ) {
-		cerr << err.what() << endl;
+    observation.setNrPeriods(args.getSwitchArgument< unsigned int >("-periods"));
+    observation.setNrBins(args.getSwitchArgument< unsigned int >("-bins"));
+	} catch  ( isa::Exceptions::SwitchNotFound &err ) {
+    std::cerr << err.what() << std::endl;
+    return 1;
+  }catch ( std::exception &err ) {
+    std::cerr << "Usage: " << argv[0] << " [-print] -opencl_platform ... -opencl_device ... -padding ... -db ... -pb ... -dt ... -pt ... -dms ... -periods ... -bins ..." << std::endl;
 		return 1;
 	}
 
-	// OpenCL Initialization
+	// Initialize OpenCL
 	cl::Context * clContext = new cl::Context();
-	vector< cl::Platform > * clPlatforms = new vector< cl::Platform >();
-	vector< cl::Device > * clDevices = new vector< cl::Device >();
-	vector< vector< cl::CommandQueue > > * clQueues = new vector< vector < cl::CommandQueue > >();
+	std::vector< cl::Platform > * clPlatforms = new std::vector< cl::Platform >();
+	std::vector< cl::Device > * clDevices = new std::vector< cl::Device >();
+	std::vector< std::vector< cl::CommandQueue > > * clQueues = new std::vector< std::vector < cl::CommandQueue > >();
 
-	initializeOpenCL(clPlatformID, 1, clPlatforms, clContext, clDevices, clQueues);
+  isa::OpenCL::initializeOpenCL(clPlatformID, 1, clPlatforms, clContext, clDevices, clQueues);
 
 	// Allocate memory
-	foldedData.allocateHostData(observation.getNrPaddedDMs() * observation.getNrBins() * observation.getNrPeriods());
-	foldedData.setCLContext(clContext);
-	foldedData.setCLQueue(&((clQueues->at(clDeviceID)).at(0)));
-	SNRData.allocateHostData(observation.getNrPeriods() * observation.getNrPaddedDMs());
-	SNRData.setCLContext(clContext);
-	SNRData.setCLQueue(&((clQueues->at(clDeviceID)).at(0)));
-	SNRDataCPU.allocateHostData(observation.getNrPeriods() * observation.getNrPaddedDMs());
-
-	try {
-		foldedData.allocateDeviceData();
-		SNRData.allocateDeviceData();
-	} catch ( OpenCLError err ) {
-		cerr << err.what() << endl;
-		return 1;
-	}
+  std::vector< dataType > foldedData = std::vector< dataType >(observation.getNrBins() * observation.getNrPeriods() * observation.getNrPaddedDMs());
+  cl::Buffer foldedData_d;
+  std::vector< dataType > snrs = std::vector< dataType >(observation.getNrPeriods() * observation.getNrPaddedDMs());
+  std::vector< dataType > snrs_c = std::vector< dataType >(observation.getNrPeriods() * observation.getNrPaddedDMs());
+  cl::Buffer snrs_d;
+  try {
+    foldedData_d = cl::Buffer(*clContext, CL_MEM_READ_WRITE, foldedData.size() * sizeof(dataType), NULL, NULL);
+    snrs_d = cl::Buffer(*clContext, CL_MEM_WRITE_ONLY, snrs.size() * sizeof(dataType), NULL, NULL);
+  } catch ( cl::Error &err ) {
+    std::cerr << "OpenCL error allocating memory: " << isa::utils::toString< cl_int >(err.err()) << "." << std::endl;
+    return 1;
+  }
 
 	srand(time(NULL));
-	for ( unsigned int bin = 0; bin < observation.getNrBins(); bin++ ) {
-		for ( unsigned int period = 0; period < observation.getNrPeriods(); period++ ) {
-			for ( unsigned int dm = 0; dm < observation.getNrDMs(); dm++ ) {
-				foldedData.setHostDataItem((bin * observation.getNrPeriods() * observation.getNrPaddedDMs()) + (period * observation.getNrPaddedDMs()) + dm, rand() % 100);
-			}
-		}
+  for ( unsigned int bin = 0; bin < observation.getNrBins(); bin++ ) {
+    for ( unsigned int period = 0; period < observation.getNrPeriods(); period++ ) {
+      for ( unsigned int DM = 0; DM < observation.getNrDMs(); DM++ ) {
+        foldedData[(bin * observation.getNrPeriods() * observation.getNrPaddedDMs()) + (period * observation.getNrPaddedDMs()) + DM] = static_cast< dataType >(rand() % 10);
+      }
+    }
 	}
+  std::fill(snrs.begin(), snrs.end(), static_cast< dataType >(0));
+  std::fill(snrs_c.begin(), snrs_c.end(), static_cast< dataType >(0));
 
-	// Generate kernel
-	SNR< dataType > clSNR("clSNR", typeName);
-	clSNR.bindOpenCL(clContext, &(clDevices->at(clDeviceID)), &((clQueues->at(clDeviceID)).at(0)));
-	clSNR.setObservation(&observation);
-	clSNR.setNrDMsPerBlock(nrDMsPerBlock);
-	clSNR.setNrPeriodsPerBlock(nrPeriodsPerBlock);
-	clSNR.setPulsarPipeline();
-	clSNR.generateCode();
+  // Copy data structures to device
+  try {
+    clQueues->at(clDeviceID)[0].enqueueWriteBuffer(foldedData_d, CL_FALSE, 0, foldedData.size() * sizeof(dataType), reinterpret_cast< void * >(foldedData.data()));
+    clQueues->at(clDeviceID)[0].enqueueWriteBuffer(snrs_d, CL_FALSE, 0, snrs.size() * sizeof(dataType), reinterpret_cast< void * >(snrs.data()));
+  } catch ( cl::Error &err ) {
+    std::cerr << "OpenCL error H2D transfer: " << isa::utils::toString< cl_int >(err.err()) << "." << std::endl;
+    return 1;
+  }
 
-	// SNR test
-	foldedData.copyHostToDevice();
-	clSNR(&foldedData, &SNRData);
-	SNRData.copyDeviceToHost();
-	pulsarSNR(observation, foldedData.getHostData(), SNRDataCPU.getHostData());
-	for ( unsigned int period = 0; period < observation.getNrPeriods(); period++ ) {
-		for ( unsigned int dm = 0; dm < observation.getNrDMs(); dm++ ) {
-			if ( !same(SNRDataCPU[(period * observation.getNrPaddedDMs()) + dm], SNRData[(period * observation.getNrPaddedDMs()) + dm]) ) {
-				wrongValues++;
-			}
-		}
-	}
+  // Generate kernel
+  cl::Kernel * kernel;
+  std::string * code = PulsarSearch::getSNROpenCL(nrDMsPerBlock, nrPeriodsPerBlock, nrDMsPerThread, nrPeriodsPerThread, typeName, observation);
+  if ( print ) {
+    std::cout << *code << std::endl;
+  }
 
-	cout << "Wrong values: " << wrongValues << " (" << setprecision(2) << (wrongValues * 100.0f) / (observation.getNrDMs() * observation.getNrPeriods()) << "%)" << endl;
-	cout << endl;
+  try {
+    kernel = isa::OpenCL::compile("snr", *code, "-cl-mad-enable -Werror", *clContext, clDevices->at(clDeviceID));
+  } catch ( isa::Exceptions::OpenCLError &err ) {
+    std::cerr << err.what() << std::endl;
+    return 1;
+  }
+
+  // Run OpenCL kernel and CPU control
+  try {
+    cl::NDRange global(observation.getNrPaddedDMs() / DMsPerThread, observation.getNrPeriods() / periodsPerThread);
+    cl::NDRange local(nrDMsPerBlock, nrPeriodsPerBlock);
+
+    kernel->setArg(0, foldedData_d);
+    kernel->setArg(1, snrs_d);
+    
+    clQueues->at(clDeviceID)[0].enqueueNDRangeKernel(*kernel, cl::NullRange, global, local, NULL, &event);
+    PulsarSearch::snrFoldedTS(observation, foldedData, snrs_c);
+  } catch ( cl::Error &err ) {
+    std::cerr << "OpenCL error: " << isa::utils::toString< cl_int >(err.err()) << "." << std::endl;
+    return 1;
+  }
+
+  for ( unsigned int dm = 0; dm < observation.getNrDMs(); dm++ ) {
+    for ( unsigned int period = 0; period < observation.getNrPeriods(); period++ ) {
+      if ( ! isa::utils::same(snrs_c[(period * observation.getNrPaddedDMs()) + dm], snrs[(period * observation.getNrPaddedDMs()) + dm]) ) {
+        wrongSamples++;
+      }
+    }
+  }
+
+  if ( wrongSamples > 0 ) {
+    std::cout << "Wrong samples: " << wrongSamples << " (" << (wrongSamples * 100.0) / (static_cast< long long unsigned int >(observation.getNrDMs()) * observation.getNrPeriods()) << "%)." << std::endl;
+  } else {
+    std::cout << "TEST PASSED." << std::endl;
+  }
 
 	return 0;
 }
